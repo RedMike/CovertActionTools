0ce9952b31cb3f4983648db576dbeb3bfde49266
Mike Roibu <contact@mikeroibu.com>
2025-03-28 00:00:00 +0000

    Initial ImGui.NET setup

--- CovertActionTools.App/Program.cs
+++ CovertActionTools.App/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CovertActionTools.App;
+
+const string title = "CovertAction.Tools";
+const int w = 800;
+const int h = 600;
+
+var renderWindow = new RenderWindow(title, w, h);
+while (renderWindow.IsOpen())
+{
+    renderWindow.ProcessInput();
+    if (!renderWindow.IsOpen())
+    {
+        break;
+    }
+    
+    //TODO: draw things
+
+    renderWindow.Render();
+}
