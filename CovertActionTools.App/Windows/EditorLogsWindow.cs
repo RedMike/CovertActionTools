a10e3a3d099fe438d4a48bcc1c6e17bd947636e1
Mike Roibu <contact@mikeroibu.com>
2025-03-29 00:00:00 +0000

    Add logs window

---
+++ CovertActionTools.App/Windows/EditorLogsWindow.cs
@@ -0,0 +1,44 @@
+﻿using System.Numerics;
+using CovertActionTools.App.ViewModels;
+using ImGuiNET;
+
+namespace CovertActionTools.App.Windows;
+
+public class EditorLogsWindow : BaseWindow
+{
+    private readonly AppLoggingState _appLoggingState;
+    private readonly MainEditorState _mainEditorState;
+
+    public EditorLogsWindow(AppLoggingState appLoggingState, MainEditorState mainEditorState)
+    {
+        _appLoggingState = appLoggingState;
+        _mainEditorState = mainEditorState;
+    }
+
+    public override void Draw()
+    {
+        if (!_mainEditorState.IsPackageLoaded)
+        {
+            return;
+        }
+        
+        var screenSize = ImGui.GetMainViewport().Size;
+        var initialPos = new Vector2(0.0f, screenSize.Y - 200.0f);
+        var initialSize = new Vector2(screenSize.X, 200.0f);
+        ImGui.SetNextWindowSize(initialSize);
+        ImGui.SetNextWindowPos(initialPos);
+        ImGui.Begin("Logs", 
+            ImGuiWindowFlags.NoResize |
+            ImGuiWindowFlags.NoMove |
+            ImGuiWindowFlags.NoNav | 
+            ImGuiWindowFlags.NoCollapse);
+        
+        var logs = _appLoggingState.Logs.ToList();
+        foreach (var log in logs)
+        {
+            ImGui.TextUnformatted(log);
+        }
+        
+        ImGui.End();
+    }
+}
