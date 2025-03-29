27b4f176f889954f3b601fb717bf7cd0168581d4
Mike Roibu <contact@mikeroibu.com>
2025-03-29 00:00:00 +0000

    Set up image window

---
+++ CovertActionTools.App/Windows/SelectedSimpleImageWindow.cs
@@ -0,0 +1,70 @@
+﻿using System.Numerics;
+using CovertActionTools.App.ViewModels;
+using CovertActionTools.Core.Models;
+using ImGuiNET;
+using Microsoft.Extensions.Logging;
+
+namespace CovertActionTools.App.Windows;
+
+public class SelectedSimpleImageWindow : BaseWindow
+{
+    private readonly ILogger<SelectedSimpleImageWindow> _logger;
+    private readonly MainEditorState _mainEditorState;
+
+    public SelectedSimpleImageWindow(ILogger<SelectedSimpleImageWindow> logger, MainEditorState mainEditorState)
+    {
+        _logger = logger;
+        _mainEditorState = mainEditorState;
+    }
+
+    public override void Draw()
+    {
+        if (!_mainEditorState.IsPackageLoaded)
+        {
+            return;
+        }
+
+        if (_mainEditorState.SelectedItem == null ||
+            _mainEditorState.SelectedItem.Value.type != MainEditorState.ItemType.SimpleImage)
+        {
+            return;
+        }
+
+        var key = _mainEditorState.SelectedItem.Value.id;
+        
+        var screenSize = ImGui.GetMainViewport().Size;
+        var initialPos = new Vector2(300.0f, 20.0f);
+        var initialSize = new Vector2(screenSize.X - 300.0f, screenSize.Y - 200.0f);
+        ImGui.SetNextWindowSize(initialSize);
+        ImGui.SetNextWindowPos(initialPos);
+        ImGui.Begin($"Image {key}",
+            ImGuiWindowFlags.NoResize |
+            ImGuiWindowFlags.NoMove |
+            ImGuiWindowFlags.NoNav | 
+            ImGuiWindowFlags.NoCollapse);
+
+        if (_mainEditorState.LoadedPackage != null)
+        {
+            var model = _mainEditorState.LoadedPackage;
+            if (model.SimpleImages.TryGetValue(key, out var image))
+            {
+                DrawImageWindow(model, key, image);
+            }
+            else
+            {
+                ImGui.Text("Something went wrong, image is missing..");
+            }
+        }
+        else
+        {
+            ImGui.Text("Something went wrong, no package loaded..");
+        }
+
+        ImGui.End();
+    }
+
+    private void DrawImageWindow(PackageModel model, string key, SimpleImageModel image)
+    {
+        
+    }
+}
