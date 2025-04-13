25e50722de57396ad06ed7c9c64fb489c2ab5762
Mike Roibu <contact@mikeroibu.com>
2025-04-13 00:00:00 +0000

    Clue window

--- CovertActionTools.App/Windows/SelectedClueWindow.cs
+++ CovertActionTools.App/Windows/SelectedClueWindow.cs
@@ -68,67 +68,147 @@ public class SelectedClueWindow : BaseWindow
     {
         //TODO: keep a pending model and have a save button?
 
+        if (ImGui.Button("Add Clue"))
+        {
+            var clue = new ClueModel();
+            if (crimeId == null)
+            {
+                clue.Type = ClueType.Weapon;
+                clue.Id = model.Clues.Where(x =>
+                        x.Value.CrimeId == null &&
+                        x.Value.Type == ClueType.Weapon)
+                    .Max(x => x.Value.Id) + 1;
+            }
+            else
+            {
+                clue.CrimeId = crimeId;
+                clue.Id = model.Clues.Where(x => x.Value.CrimeId == crimeId)
+                    .Max(x => x.Value.Id) + 1;
+            }
+            model.Clues.Add(clue.GetMessagePrefix(), clue);
+        }
         var clues = model.Clues.Values
             .Where(x => x.CrimeId == crimeId)
             .OrderBy(x => x.CrimeId != null ? x.Id : (int)x.Type)
             .ThenBy(x => x.Id)
             .ToList();
-        var i = 0;
-        foreach (var clue in clues)
+        for (var i = 0; i < clues.Count; i++)
         {
-            ImGui.PushID($"Clue{i++}");
-            
+            ImGui.PushID($"Clue_{i}");
+            DrawClue(model, clues[i], i);
+            ImGui.Text("");
+            ImGui.PopID();
+        }
+    }
+
+    private void DrawClue(PackageModel model, ClueModel clue, int i)
+    {
+        var windowSize = ImGui.GetContentRegionAvail();
+        
+        ImGui.BeginChild($"Clue {clue.GetMessagePrefix()}", new Vector2(windowSize.X, 130.0f), true);
+        
+        var cursorPos = ImGui.GetCursorPos();
+        ImGui.Text($"Clue {clue.GetMessagePrefix()}");
+        var nextCursorPos = ImGui.GetCursorPos();
+        
+        //now move to the right to make the delete button
+        var o = ImGui.CalcTextSize(" Remove ");
+        ImGui.SetCursorPos(new Vector2(windowSize.X - o.X, cursorPos.Y));
+        if (ImGui.Button("Remove"))
+        {
+            model.Clues.Remove(clue.GetMessagePrefix());
+            return;
+        }
+        
+        ImGui.SetCursorPos(nextCursorPos);
+        ImGui.Text("");
+
+        if (ImGui.BeginTable("c1", 4))
+        {
+            ImGui.TableNextRow();
+            ImGui.TableNextColumn();
             //non-crime clues aren't tied to any crime
             var newCrimeId = ImGuiExtensions.Input("Crime ID", clue.CrimeId ?? -1, width: 100);
             if (newCrimeId != null)
             {
-                //TODO: change crime ID
-            }
-            ImGuiExtensions.SameLineSpace();
-
-            var newId = ImGuiExtensions.Input("ID", clue.Id, width: 100);
-            if (newId != null)
-            {
-                if (model.Texts.Any(x => x.Value.Id == newId && x.Value.CrimeId == crimeId))
+                var oldPrefix = clue.GetMessagePrefix();
+                var oldCrimeId = clue.CrimeId;
+                clue.CrimeId = newCrimeId;
+                var newPrefix = clue.GetMessagePrefix();
+                if (model.Clues.ContainsKey(newPrefix))
                 {
-                    ImGui.SameLine();
-                    ImGui.Text("Key already taken");
+                    //TODO: error
+                    clue.CrimeId = oldCrimeId;
                 }
                 else
                 {
-                    //TODO: change ID?
+                    model.Clues.Remove(oldPrefix);
+                    model.Clues.Add(newPrefix, clue);
                 }
             }
-            
-            ImGuiExtensions.SameLineSpace();
 
+            ImGui.TableNextColumn();
+            var newId = ImGuiExtensions.Input("ID", clue.Id, width: 100);
+            if (newId != null)
+            {
+                var oldPrefix = clue.GetMessagePrefix();
+                var oldId = clue.Id;
+                clue.Id = newId.Value;
+                var newPrefix = clue.GetMessagePrefix();
+                if (model.Clues.ContainsKey(newPrefix))
+                {
+                    //TODO: error
+                    clue.Id = oldId;
+                }
+                else
+                {
+                    model.Clues.Remove(oldPrefix);
+                    model.Clues.Add(newPrefix, clue);
+                }
+            }
+
+            ImGui.TableNextColumn();
             var newClueType = ImGuiExtensions.InputEnum("Clue Type", clue.Type, false, ClueType.Unknown, width: 150);
             if (newClueType != null)
             {
-                //TODO: change
+                var oldPrefix = clue.GetMessagePrefix();
+                var oldType = clue.Type;
+                clue.Type = newClueType.Value;
+                var newPrefix = clue.GetMessagePrefix();
+                if (model.Clues.ContainsKey(newPrefix))
+                {
+                    //TODO: error
+                    clue.Type = oldType;
+                }
+                else
+                {
+                    model.Clues.Remove(oldPrefix);
+                    model.Clues.Add(newPrefix, clue);
+                }
             }
-            
-            ImGuiExtensions.SameLineSpace();
 
-            var newClueSource = ImGuiExtensions.InputEnum("Source", clue.Source, false, ClueModel.ClueSource.Unknown, width: 150);
+
+            ImGui.TableNextColumn();
+            var newClueSource =
+                ImGuiExtensions.InputEnum("Source", clue.Source, false, ClueModel.ClueSource.Unknown, width: 150);
             if (newClueSource != null)
             {
-                //TODO: change
+                clue.Source = newClueSource.Value;
             }
-            
-            var windowSize = ImGui.GetContentRegionAvail();
-            var message = clue.Message.Replace("\r", ""); //strip out \r and re-add after, for consistency across OS
-            var origMessage = message;
-            ImGui.InputTextMultiline($"Message {clue.GetMessagePrefix()}", ref message, 1024, new Vector2(windowSize.X, 30.0f),
-                ImGuiInputTextFlags.NoHorizontalScroll | ImGuiInputTextFlags.CtrlEnterForNewLine);
-            if (message != origMessage)
-            {
-                var fixedMessage = message.Replace("\n", "\r\n"); //re-add \r, for consistency across OS
-                //TODO: change message
-            }
-            
-            ImGui.Separator();
-            ImGui.PopID();
+            ImGui.EndTable();
         }
+        
+        var message = clue.Message.Replace("\r", ""); //strip out \r and re-add after, for consistency across OS
+        var origMessage = message;
+        ImGui.InputTextMultiline($"Message {clue.GetMessagePrefix()}", ref message, 1024,
+            new Vector2(windowSize.X, 50.0f),
+            ImGuiInputTextFlags.NoHorizontalScroll | ImGuiInputTextFlags.CtrlEnterForNewLine);
+        if (message != origMessage)
+        {
+            var fixedMessage = message.Replace("\n", "\r\n"); //re-add \r, for consistency across OS
+            clue.Message = fixedMessage;
+        }
+
+        ImGui.EndChild();
     }
 }
